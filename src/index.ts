3b04ae6a2554d599a6eca98abf0e7a14ba2df238
Pablo Arce <josepa_arce@hotmail.com>
2023-11-01 23:40:49 -0600

    init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import gamesRouter from './routes/game.routes';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json()); // Enables JSON parsing in the request body
+app.use('/games', gamesRouter);
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript Express!");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
